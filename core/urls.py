7beb9830df7f0ac0e78817def96cc6a1f5bc9ce4
xamidovscode <xamidovcoder@gmail.com>
2025-06-05 17:56:43 +0500

    settings module

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Optional UI:
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('docs/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
 ]
