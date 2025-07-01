edd2e1c2af1e2d131c1ed94658c60bada9fa8992
xamidovscode <xamidovcoder@gmail.com>
2025-07-01 14:46:14 +0500

    some news

--- core/settings/base.py
+++ core/settings/base.py
@@ -1,7 +1,10 @@
+import os
+
 from pathlib import Path
 from decouple import config
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
 
 SECRET_KEY = config('SECRET_KEY', default='some-secret-key', cast=str)
 DEBUG = config('DEBUG', default=False, cast=bool)
@@ -153,7 +156,6 @@ USE_TZ = True
 USE_L10N = True
 
 
-STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
@@ -173,3 +175,12 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
